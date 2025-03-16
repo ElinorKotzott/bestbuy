335e46e567ea848488cb14c419349eaf57833895
Eli_Nor <elinor.kotzott@gmx.de>
2025-03-16 16:59:33 +0100

    added docstrings, fixed calls of instance methods and encapsulation problem

--- products.py
+++ products.py
@@ -1,6 +1,7 @@
 class Product:
 
     def __init__(self, name, price, quantity):
+        """instantiates Product"""
         if not name or price < 0 or quantity < 0:
             raise ValueError("Invalid input: 'name' cannot be empty, and 'price' and 'quantity' cannot be negative!")
         self.name = name
@@ -10,34 +11,41 @@ class Product:
 
 
     def get_quantity(self):
+        """returns quantity"""
         return self.quantity
 
 
     def set_quantity(self, quantity):
+        """sets quantity and deactivates product if quantity is zero"""
         if quantity == 0:
-            Product.deactivate(self)
+            self.deactivate()
         self.quantity = quantity
 
 
     def activate(self):
+        """activates product"""
         self.active = True
 
 
     def deactivate(self):
+        """deactivates product"""
         self.active = False
 
 
     def is_active(self):
+        """returns whether the product is active"""
         return self.active
 
 
     def show(self):
+        """string representation of a product"""
         return f"{self.name}, Price: ${self.price}, Quantity: {self.quantity}"
 
 
     def buy(self, quantity):
+        """buys the product if enough quantity is available and updates quantity. returns total price"""
         print(f"Buying {quantity}x {self.name}...")
         if quantity > self.quantity:
             raise ValueError(f"Not that many {self.name}s available")
-        Product.set_quantity(self, self.quantity - quantity)
+        self.set_quantity(self.quantity - quantity)
         return quantity * self.price
--- store.py
+++ store.py
@@ -1,19 +1,27 @@
 class Store:
 
     def __init__(self, products_list):
+        """instantiates store with a list of objects (products)"""
         self.products_list = products_list
 
 
     def add_product(self, product):
+        """adds a product to the list of products"""
         self.products_list.append(product)
 
 
     def remove_product(self, product):
+        """removes a product from the list of products"""
         if product in self.products_list:
             self.products_list.remove(product)
 
 
+    def get_products_list(self):
+        """returns list of products"""
+        return self.products_list
+
     def get_total_quantity(self):
+        """returns total amount of products available in the store"""
         total_amount = 0
         for item in self.products_list:
             total_amount = total_amount + item.quantity
@@ -21,6 +29,7 @@ class Store:
 
 
     def get_all_products(self):
+        """returns active products i. e. products with a quantity higher than zero"""
         active_products = []
         for product in self.products_list:
             if product.is_active():
@@ -29,6 +38,7 @@ class Store:
 
 
     def order(self, shopping_list):
+        """calls buy on all items in our shopping list and then returns the total price of the order"""
         total_price = 0
         for item in shopping_list:
             total_price = total_price + item[0].buy(item[1])
